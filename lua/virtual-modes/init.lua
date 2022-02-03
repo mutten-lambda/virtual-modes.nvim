bbf199ed75177538e657c1f1f741f7bcf2e4b82c
mutten <mutten-gitlab@protonmail.com>
2022-02-03 17:33:43 +0100

    Add input validation logic

--- lua/virtual-modes/init.lua
+++ lua/virtual-modes/init.lua
@@ -4,51 +4,32 @@ local normal_mode = "NORMAL"
 local current_mode = normal_mode
 local virtual_modes = {}
 local notify = require("virtual-modes.utils.notify").notify
-local validator = require("virtual-modes.validate")
+local validate = require("virtual-modes.validate")
 local global_defaults = {
 	keymap_enter_prefix = "",
+	enable_keymap_prefix = true,
 	keymap_exit = "<esc>",
-	on_enter = function()
-		notify("Entered mode " .. M.get_virtual_mode())
-	end,
-	on_exit = function()
-		notify("Exiting mode " .. M.get_virtual_mode())
-	end,
+	on_enter = {
+		function()
+			notify("Entered mode " .. M.get_virtual_mode())
+		end,
+	},
+	on_exit = {
+		function()
+			notify("Exiting mode " .. M.get_virtual_mode())
+		end,
+	},
 }
 
-local function init()
-	notify("Setting up virtual-modes!", "debug")
-	M.setup({
-		keymap_enter_prefix = "<leader>e",
-		modes = {
-			GIT = {
-				keymap_enter = "g",
-			},
-			TEST = {
-				keymap_enter = "t",
-			},
-		},
-	})
-	M.print_modes()
-end
-
--- DEBUG
-function M.reload()
-	package.loaded["virtual-modes"] = nil
-	require("virtual-modes")
-end
-
-function M.print_names()
-	vim.notify("The recognized modes are:\n" .. vim.inspect(M.get_mode_names()), "debug")
-end
-
-function M.print_modes()
-	vim.notify("The mode settings are:\n" .. vim.inspect(virtual_modes), "debug")
-end
-
 -- Mode utility functions
+-- TODO check if other modules can change the returned values. This would be unsafe!
+-- Otherwise make local or move to other module.
 function M.get_virtual_mode()
 	return current_mode
+end
+
+function M.get_mode_configs()
+	return virtual_modes
 end
 
 function M.mode_is_available(name)
@@ -75,39 +56,52 @@ local function exec_string_or_function(value)
 	end
 end
 
+-- Execute vim command strings or lua functions, possible in a table.
+-- Everything else gets ignored.
+local function exec_string_or_function_table(value)
+	if type(value) ~= "table" then
+		exec_string_or_function(value)
+	else
+		for _, v in ipairs(value) do
+			exec_string_or_function(v)
+		end
+	end
+end
+
+-- Add missing fields the default value.
 local function add_defaults(mode_config)
 	local c = mode_config
-	c.keymap_enter = global_defaults.keymap_enter_prefix .. c.keymap_enter
-	c.on_enter = c.on_enter or global_defaults.on_enter -- TODO both local and global
-	c.on_exit = c.on_exit or global_defaults.on_exit -- TODO both local and global
+	local gd = global_defaults
+	c.enable_keymap_prefix = c.enable_keymap_prefix or gd.enable_keymap_prefix
+	c.keymap_enter = gd.keymap_enter_prefix .. c.keymap_enter -- TODO use enable_keymap_prefix
+	c.on_enter = c.on_enter or gd.on_enter -- TODO both local and global
+	c.on_exit = c.on_exit or gd.on_exit -- TODO both local and global
 	return c
 end
 
 -- Add a mode. Overwrite if mode already exists.
-function M.add_mode(mode_config)
+local function add_mode(mode_config)
 	mode_config = add_defaults(mode_config)
-	if not validator.is_valid_mode_config(mode_config) then
-		validator.print_mode_config_warning(mode_config)
-	else
-		local c = mode_config
-		local name = c.name
-		-- Actually declaring them makes sure no unrecognized fields get past
-		virtual_modes[name] = {
-			keymap_enter = c.keymap_enter,
-			on_enter = c.on_enter,
-			on_exit = c.on_exit,
-		}
+	local c = mode_config
+	local name = c.name
 
-		-- Setting keymaps
-		local opts = { noremap = true }
-		vim.api.nvim_set_keymap("n", "<esc>", "<cmd>lua require('virtual-modes').exit_mode()<cr><esc>", opts)
-		vim.api.nvim_set_keymap(
-			"n",
-			virtual_modes[name].keymap_enter,
-			"<cmd>lua require('virtual-modes').enter_mode('" .. name .. "')<cr>",
-			opts
-		)
-	end
+	-- Actually declaring them makes sure no unrecognized fields get past
+	virtual_modes[name] = {
+		keymap_enter = c.keymap_enter,
+		enable_keymap_prefix = c.enable_keymap_prefix,
+		on_enter = c.on_enter,
+		on_exit = c.on_exit,
+	}
+
+	-- Setting keymaps
+	local opts = { noremap = true }
+	vim.api.nvim_set_keymap("n", "<esc>", "<cmd>lua require('virtual-modes').exit_mode()<cr><esc>", opts)
+	vim.api.nvim_set_keymap(
+		"n",
+		virtual_modes[name].keymap_enter,
+		"<cmd>lua require('virtual-modes').enter_mode('" .. name .. "')<cr>",
+		opts
+	)
 end
 
 -- Remove a mode, if it exists.
@@ -126,34 +120,39 @@ function M.enter_mode(name)
 			M.exit_mode()
 		end
 		current_mode = name
-		exec_string_or_function(virtual_modes[name].on_enter)
+		exec_string_or_function_table(virtual_modes[name].on_enter)
 		-- set_keymaps(name)
 	end
 end
 
 function M.exit_mode()
 	if current_mode ~= normal_mode then
-		exec_string_or_function(virtual_modes[current_mode].on_exit)
+		exec_string_or_function_table(virtual_modes[current_mode].on_exit)
 		current_mode = normal_mode
 	end
 end
 
 function M.setup(config)
-	-- Change global defaults
-	global_defaults.keymap_enter_prefix = config.keymap_enter_prefix or global_defaults.keymap_enter_prefix
-
-	-- Add all modes
-	local modes = config.modes or {}
-	if type(modes) ~= "table" then
-		notify("Field 'modes' should be a table, not: " .. type(table), "warn")
+	-- TODO validate input: on_* should be table
+	if not validate.is_config(config) then
+		validate.print_config_warning(config)
 	else
+		-- Change global defaults
+		local gd = global_defaults
+		gd.keymap_enter_prefix = config.keymap_enter_prefix or gd.keymap_enter_prefix
+		gd.enable_keymap_prefix = config.enable_keymap_prefix or gd.enable_keymap_prefix
+		gd.on_enter = config.on_enter or gd.on_enter
+		gd.on_exit = config.on_exit or gd.on_exit
+
+		-- Add all modes
+		local modes = config.modes or {}
 		for name, mode in pairs(modes) do
 			mode.name = mode.name or name -- Use key if name is not set.
-			M.add_mode(mode)
+			add_mode(mode)
 		end
 	end
 end
 
-init()
+require("virtual-modes.debug").init_test(M)
 
 return M
--- lua/virtual-modes/validate.lua
+++ lua/virtual-modes/validate.lua
@@ -3,31 +3,161 @@ local M = {}
 local normal_mode = "NORMAL" -- TODO fix code duplication (see init.lua)
 local notify = require("virtual-modes.utils.notify").notify
 
-local function is_string_or_function(value)
+-- Validating functions
+local function is_bool(value)
+	return type(value) == "boolean"
+end
+
+local function is_string(value)
+	return type(value) == "string"
+end
+
+local function is_simple_executable(value)
 	return type(value) == "string" or type(value) == "function"
 end
 
-function M.is_valid_mode_config(mode_config)
-	local c = mode_config
-	return type(c.name) == "string"
-		and c.name ~= normal_mode
-		and type(c.keymap_enter) == "string"
-		and is_string_or_function(c.on_enter)
-		and is_string_or_function(c.on_exit)
+local function is_table_of_executables(value)
+	local result = true
+	if type(value) ~= "table" then
+		result = false
+	else
+		for _, v in ipairs(value) do
+			result = result and is_simple_executable(v)
+		end
+	end
+	return result
 end
 
-function M.print_mode_config_warning(mode_config)
-	local c = mode_config
-	if not type(c.name) ~= "string" then
-		notify("Mode should have a name.", "warn")
-	elseif c.name == normal_mode then
+local function is_executable(value)
+	return is_simple_executable(value) or is_table_of_executables()
+end
+
+local is_valid = {
+	name = is_string,
+	keymap_enter = is_string,
+	keymap_enter_prefix = is_string,
+	enable_keymap_prefix = is_bool,
+	on_enter = is_executable,
+	on_exit = is_executable,
+	modes = nil, -- cannot be set yet since the defining function uses the is_valid table
+}
+
+-- TODO make local
+function M.is_mode(mode_config)
+	local result = false
+	if type(mode_config) == "table" then
+		result = true
+		for key, value in pairs(mode_config) do
+			if type(is_valid[key]) == "function" then
+				result = result and is_valid[key](value)
+			else
+				notify("Expected a function to validate " .. key .. ", but got: " .. type(is_valid[key]), "debug")
+			end
+		end
+	end
+	-- return result
+	return true
+end
+
+local function is_table_of_modes(value)
+	local result = false
+	if type(value) == "table" then
+		result = true
+		for _, mode_config in pairs(value) do
+			result = result and M.is_mode(mode_config)
+		end
+	end
+	return result
+end
+
+is_valid.modes = is_table_of_modes -- Complete table
+-- M.is_mode_config = is_mode
+
+-- Verify type constraints. Unknown keys are ignored.
+function M.is_config(config)
+	local result = false
+	if type(config) == "table" then
+		result = true
+		for key, value in pairs(config) do
+			if type(is_valid[key]) == "function" then
+				result = result and is_valid[key](value)
+			else
+				notify("Expected a function to validate " .. key .. ", but got: " .. type(is_valid[key]), "debug")
+			end
+		end
+	end
+	return result
+end
+
+-- Print functions
+local function should_be_type(t, key, value)
+	notify("Field '" .. key .. "' must be a " .. t .. ".\nGot a: " .. type(value) .. ".", "warn")
+end
+
+local function should_be_string(key, value)
+	should_be_type("string", key, value)
+end
+
+local function should_be_name(key, value)
+	if not is_string(value) then
+		should_be_string(key, value)
+	elseif value == normal_mode then
 		notify("Mode cannot be named '" .. normal_mode .. "'.", "warn")
-	elseif type(c.keymap_enter) ~="string" then
-		notify("Field keymap_enter must be a string, not: " .. type(c.keymap_enter), "warn")
-	elseif not is_string_or_function(c.on_enter) then
-		notify("Field 'on_enter' must be a string or lua function, not: " .. type(c.on_enter), "warn")
-	elseif not is_string_or_function(c.on_exit) then
-		notify("Field 'on_exit' must be a string or lua function, not: " .. type(c.on_exit), "warn")
+	end
+end
+
+local function should_be_bool(key, value)
+	should_be_type("boolean", key, value)
+end
+
+local function should_be_executable(key, value)
+	notify(
+		"Field '"
+			.. key
+			.. "' must be a string, a lua function or table containing strings and/or lua functions.\nGot a: "
+			.. type(value)
+			.. ".",
+		"warn"
+	)
+end
+
+local print_warning = {
+	name = should_be_name,
+	keymap_enter = should_be_string,
+	keymap_enter_prefix = should_be_string,
+	enable_keymap_prefix = should_be_bool,
+	on_enter = should_be_executable,
+	on_exit = should_be_executable,
+	modes = nil, -- cannot be set yet since the defining function uses the is_valid table
+}
+
+function M.print_mode_config_warning(name, mode_config)
+	if type(mode_config) ~= "table" then
+		notify(name .. "mode config should be a table", "warn")
+	else
+		for key, value in pairs(mode_config) do
+			if type(is_valid[key]) == "function" and not is_valid[key](value) then
+				print_warning[key](key, value)
+			else
+				notify("Unknown field: " .. key .. ".", "warn")
+			end
+		end
+	end
+end
+
+print_warning.modes = M.print_mode_config_warning
+
+function M.print_config_warning(config)
+	if type(config) ~= "table" then
+		notify("Global config should be a table", "warn")
+	else
+		for key, value in pairs(config) do
+			if type(is_valid[key]) == "function" and not is_valid[key](value) then
+				print_warning[key](key, value)
+			else
+				notify("Unknown field: " .. key .. ".", "warn")
+			end
+		end
 	end
 end
 
